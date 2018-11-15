edf7d6293ab8267739aef6f69534e641596e7051
Franz Wilding <franz.wilding@me.com>
2018-11-15 17:25:50 +0100

    refactored most of the mixin, use original sortable methods as early as possible, extend README

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 Uses UIkit.mixin to add a **nestable** option to the uikit3 sortable component.
 
-**NOT FULLY PRODUCTION READY!** There are some animation problemes at the moment, that makes it sometimes hard to drop an item as a child.
-
 ## Installation 
 
     yarn add uikit3-nestable
@@ -16,6 +14,58 @@ Uses UIkit.mixin to add a **nestable** option to the uikit3 sortable component.
 
 ## Usage
 
-After including node_moduels/uikit3-nestable/dist/js/nestable.js, you have a new sortable option: 
+After including **node_moduels/uikit3-nestable/dist/js/nestable.js**, you have a new sortable option: 
 
+```html
     <div uk-sortable="handle: .uk-sortable-handle; nestable: true"></div>
+````
+
+In order to make nestable work, you must add at least the following css code: 
+
+```css
+    /* nested sortable groups indention */            
+    .uk-sortable .uk-sortable,
+    .uk-sortable-drag .uk-sortable {
+        margin-left: 50px;
+    }
+
+    /* nested sortable groups inner padding to the parent element */
+    .uk-sortable .uk-sortable:not(.uk-sortable-empty),
+    .uk-sortable .uk-sortable-drag:not(.uk-sortable-empty) {
+        padding-top: 20px;
+    }
+
+    /* remove uk-sortable-empty min-height sort nested sortables */
+    .uk-sortable .uk-sortable.uk-sortable-empty,
+    .uk-sortable-drag .uk-sortable.uk-sortable-empty {
+        min-height: 0;
+    }
+```
+
+To make it easier to sort, you should make the placeholder visible. Something like this (taken from uikit2): 
+
+```css
+    /* Custom placeholder styles, makes it easier to see the current position */
+    .uk-sortable-placeholder {
+        position: relative;
+        opacity: 1;
+    }
+
+    .uk-sortable-placeholder > * {
+        opacity: 0;
+    }
+
+    .uk-sortable-placeholder:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        border: 1px dashed #E5E5E5;
+        opacity: 1;
+    }
+```
+
+A working example can be found in tests/index.html
+Please see tests/index.html
--- src/js/nestable.js
+++ src/js/nestable.js
@@ -2,107 +2,88 @@
 import sortable from 'uikit/src/js/components/sortable';
 import {getPos, includes, each, append, toNodes, within} from 'uikit-util';
 
+let originalMove = sortable.methods.move;
+let originalConnected = sortable.methods.connected;
+
 UIkit.mixin({
-    nestableMargin: 40,
+    nestableMargin: 50,
     props: {
         nestable: Boolean
     },
     data: {
         nestable: false
     },
-    ready() {
+    connected() {
         if(this.nestable) {
+
+            // Create nestable sortable group with the same props as this sortable.
+            let propsArray = Object.keys(this.$props).map((key) => { return key + ':' + this.$props[key]; });
             each(this.$el.children, (child) => {
                 if(typeof child == 'object') {
-                    child.ukSortableNestableGroup = append(child, '<div style="margin-left:' + this.$options.nestableMargin + 'px;" uk-sortable="handle: .uk-sortable-handle; nestable: true"></div>');
+                    child.querySelector('[uk-sortable]') || append(child, '<div uk-sortable="' + propsArray.join(';') + '"></div>');
                 }
             });
         }
+
+        // Call original connected method.
+        originalConnected ? originalConnected.call(this) : null;
     },
     methods: {
+
+        // Move the item inside the nestable tree (called by move()).
+        nestedMove(source, destination) {
+            if(!destination) {
+                return false;
+            }
+
+            destination = this.getSortable(destination);
+            if(destination) {
+                if(source != destination) {
+                    destination.insert(this.placeholder);
+                    if (!includes(this.touched, destination)) {
+                        this.touched.push(destination);
+                    }
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
         move(e) {
 
-            if (!this.drag) {
-
-                if (Math.abs(this.pos.x - this.origin.x) > this.threshold || Math.abs(this.pos.y - this.origin.y) > this.threshold) {
-                    this.start(e);
-                }
-
-                return;
-            }
-
-            this.$emit();
-
-            let target = e.type === 'mousemove' ? e.target : document.elementFromPoint(this.pos.x - document.body.scrollLeft, this.pos.y - document.body.scrollTop);
-
-            //console.log(e.target);
-
-
+            const target = e.type === 'mousemove' ? e.target : document.elementFromPoint(this.pos.x - document.body.scrollLeft, this.pos.y - document.body.scrollTop);
             const sortable = this.getSortable(target);
-            const previous = this.getSortable(this.placeholder);
-            const move = sortable !== previous;
 
-            let move_to_group = false;
-            let move_to_child = false;
-            let move_to_parent = false;
+            if(this.drag && sortable) {
 
-            // If sortable is not defined, be cannot drop item here.
-            if(!sortable) {
-                return;
-            }
+                if(this.drag.getBoundingClientRect().left - target.getBoundingClientRect().left > this.$options.nestableMargin) {
 
-            // If target was moved, we need a previous sortable object
-            if(move && !previous) {
-                return;
-            }            
-
-            // If we move the item to another sortable, this is only allowed 
-            // if they have the same group or sortable is a child of previous
-            // and previous is nestable
-            if(move) {
-
-                if(sortable.group && sortable.group === previous.group) {
-                    move_to_group  = true;
-                }
-
-                if(sortable.nestable && !within(sortable.$el, this.placeholder)) {
-                    if(within(sortable.$el, previous.$el)) {
-                        move_to_child = true;
+                    // We are dragging inside the current element
+                    if(target.previousElementSibling) {
+                        if(this.nestedMove(sortable, target.previousElementSibling.querySelector('[uk-sortable]'))) {
+                            return;
+                        }
                     }
-    
-                    if(within(previous.$el, sortable.$el)) {
-                        if(this.drag.getBoundingClientRect().left - sortable.$el.getBoundingClientRect().left < this.$options.nestableMargin) {
-                            move_to_parent = true;
+                    
+                    // We are dragging into another item's child group
+                    else if(target.parentElement && (this.getSortable(this.placeholder) !== this.getSortable(target.parentElement))) {
+                        if(this.nestedMove(sortable, target.parentElement.querySelector('[uk-sortable]'))) {
+                            return;
                         }
                     }
                 }
 
-                if(!move_to_group && !move_to_child && !move_to_parent) {
-                    return;
+                // We are dragging the element to another branch of the tree
+                else if(target !== this.placeholder) {
+                    if(this.nestedMove(this.getSortable(this.placeholder), target)) {
+                        return;
+                    }
                 }
             }
 
-            target = sortable.$el === target.parentNode && target || toNodes(sortable.$el.children).filter(element => within(target, element))[0];            
-
-            if (move) {
-                previous.remove(this.placeholder);
-            } else if (!target) {
-                return;
-            }
-
-            if(move_to_child) {
-                console.log("move_to_child", target);
-            }
-
-            if(move_to_parent) {
-                console.log("move_to_parent", target);
-            }
-
-            sortable.insert(this.placeholder, target);
-
-            if (!includes(this.touched, sortable)) {
-                this.touched.push(sortable);
-            }
+            // Handle original move
+            originalMove ? originalMove.call(this, e) : null;
         }
     }
 }, 'sortable');
